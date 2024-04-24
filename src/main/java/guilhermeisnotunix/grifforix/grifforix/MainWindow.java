61dab90e2460066fa4652456afa8d5ef7fb3d21c
Faura <36975422+GuilhermeIsNotUnix@users.noreply.github.com>
2024-04-24 17:08:41 -0300

    Added documentation on the class

--- src/main/java/guilhermeisnotunix/grifforix/grifforix/MainWindow.java
+++ src/main/java/guilhermeisnotunix/grifforix/grifforix/MainWindow.java
@@ -9,6 +9,19 @@ import java.io.IOException;
 
 import static guilhermeisnotunix.grifforix.password.Password.*;
 
+/** <p>This class represents the main grifforix window, in which JavaFX for the GUI is used.</p>
+ * <p>Some procedures are specific and traditional implementations of JavaFX, such as the class inheritance presented below inheriting the Application class and the @Override of the start method (method in which the application's fxml is loaded and the main window is configured and displayed)...</p>
+ * <p>Furthermore, inside the main method there is a simple command line interface, in case someone wants to run grifforix without exactly using graphical mode, also allowing to use some basic grifforix functionalities via the command line with arguments. If the application is started without arguments (clicking on a grifforix executable, a .exe on Windows or .rpm on Fedora Linux for example, also means starting it without arguments), it will automatically start in graphical mode with its JavaFX graphical interface. However, if the user runs grifforix from the command line with arguments, for example: ./grifforix -V, then the graphical interface will not be started, but rather the functionality will be executed directly via the command line.</p>
+ * <p>For now there are only two commands possible to execute:</p>
+ * <p>1. "./grifforix -V": Prints the griforix version.</p>
+ * <p>2. "./grifforix -n": Generates a random password with a fixed length of 20 characters.</p>
+ *
+ * @author Guilherme Faura
+ * @author <a href="https://guilhermeisnotunix.github.io">guilhermeisnotunix</a>
+ * @author <a href="https://github.com/guilhermeisnotunix">GitHub</a>
+ * @version 0.9.2-alpha
+ */
+
 public class MainWindow extends Application {
 
     @Override
